36c0cc0b0efa76238a4e7dfe80900f39b78d9ba5
Gilles Helleu <helleugilles@gmail.com>
2023-12-09 10:17:34 +0100

    ajout aggrégations finies

--- aggregation/ag_4.py
+++ aggregation/ag_4.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 
-# Les 5 réalisateurs ayant le plus de films
+# la liste et le nombre
+# de films des 15 acteurs les plus présents
 # from module.connexion import movies
 client = MongoClient("mongodb://localhost:27017/")
 # Base de donnée Cinema
@@ -8,25 +9,41 @@ db = client["cinema"]
 # Collection movies
 movies = db["movies"]
 
-ag_3 = movies.aggregate(
+ag_4 = movies.aggregate(
     [
         {
+            '$addFields': {
+                'new_actor': {
+                    '$split': [
+                        '$Cast', '|'
+                    ]
+                }
+            }
+        }, {
+            '$unwind': {
+                'path': '$new_actor'
+            }
+        }, {
             '$group': {
-                '_id': '$Director',
-                'movies_number': {
+                '_id': '$new_actor',
+                'count': {
                     '$sum': 1
+                },
+                'films': {
+                    '$push': '$Title'
                 }
             }
         }, {
             '$sort': {
-                'movies_number': -1
+                'count': -1
             }
-        }, {
-            '$limit': 5
+        },
+        {
+            '$limit': 15
         }
     ]
 )
 
 if __name__ == "__main__":
-    for t in ag_3:
+    for t in ag_4:
         print(t)
